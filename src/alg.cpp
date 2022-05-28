5601865706c2db67e7a6b9bfc7b215f6bbabb3d9
Водолагин Артём <52680620+Xainqq@users.noreply.github.com>
2022-05-28 18:56:10 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,31 @@
 // Copyright 2021 NNTU-CS
 #include  <iostream>
 #include  <fstream>
+#include  <string>
 #include  <locale>
 #include  <cstdlib>
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
   // поместите сюда свой код
+   BST<std::string> bst;
+  std::ifstream file(filename);
+  std::string word = "";
+  char lowercase = 'a' - 'A';
+  if (!file.is_open()) {
+    throw std::string("Error! the file was not opened.");
+  }
+  while (!file.eof()) {
+    char temp = file.get();
+    if ((temp >= 'a' && temp <= 'z') || (temp >= 'A' && temp <= 'Z')) {
+      if (temp >= 'A' && temp <= 'Z')
+        temp += lowercase;
+      word += temp;
+    } else if (word != "") {
+      bst.add(word);
+      word = "";
+    }
+  }
+  file.close();
+  return bst;
 }
